17f28587b4e35946190b931a5ade83cacf1b2dab
Pavel Halabala <halabap@email.cz>
2023-09-04 14:10:17 +0200

    fix

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ export AZ_RESOURCE_GROUP=gma-stream-infra
 export AZ_DATABASE_NAME=spring-data-jdbc-test
 export AZ_LOCATION=westeu
 export AZ_SQL_SERVER_USERNAME=HCI-AZ-ADM-Halabala@embedit.cz
-export AZ_SQL_SERVER_PASSWORD=halabala123!
+export AZ_SQL_SERVER_PASSWORD=
 export AZ_LOCAL_IP_ADDRESS=$(curl http://whatismyip.akamai.com/)
 
 #export SPRING_DATASOURCE_URL="jdbc:sqlserver://$AZ_DATABASE_NAME.database.windows.net:1433;database=demo;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
